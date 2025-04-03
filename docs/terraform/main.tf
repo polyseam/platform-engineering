41db3cb28d883022e4c6f8d71d0eae80eb24c9c5
david <hurleyldave@gmailcom>
2025-04-03 12:23:39 -0600

    update terraform

--- docs/terraform/main.tf
+++ docs/terraform/main.tf
@@ -13,8 +13,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "TestStorageAccount"
-  resource_group_name      = "ExampleResourceGroup"
+  name                     = "davidteststorage"
+  resource_group_name      = "david-personal"
   location                 = "West US"
   account_tier             = "Standard"
   account_replication_type = "LRS"
