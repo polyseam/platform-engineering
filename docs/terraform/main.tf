66d00f193cf504c097c845fb1504e3b41d68be9b
David Hurley <34819526+david-hurley@users.noreply.github.com>
2025-04-03 14:58:06 -0600

    Update main.tf
    add comments to tf

--- docs/terraform/main.tf
+++ docs/terraform/main.tf
@@ -13,8 +13,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "terraformtesting123456"
-  resource_group_name      = "david-personal"
+  name                     = "terraformtesting123456" # use whatever name you like
+  resource_group_name      = "my-rg" # should be a existing RG
   location                 = "West US"
   account_tier             = "Standard"
   account_replication_type = "LRS"
